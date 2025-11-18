e974a865560a3e3568f504349ca319966bf2eaeb
hanum <dlgksdma15@naver.com>
2025-11-18 19:04:39 +0900

    [fix]: bean 등록으로 설정

---
+++ src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterLocateConfig {
+
+    @Bean
+    public RouteLocator myRoute(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("coupon",
+                        p->p.path("/coupons/**").and()
+                                .uri("lb://COUPON")).build();
+    }
+}
