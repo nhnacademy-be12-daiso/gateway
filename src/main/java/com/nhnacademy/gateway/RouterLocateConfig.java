8fe49319311bbf55b5a783027db6ade31ce87d70
hanum <dlgksdma15@naver.com>
2025-11-25 13:34:27 +0900

    [fix]: gateway config 변경

--- src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
+++ src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
@@ -12,15 +12,15 @@ public class RouterLocateConfig {
     public RouteLocator myRoute(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("team3-auth",
-                        p->p.path("/auth/**")
-                        .uri("lb://TEAM3-AUTH")).build();
+                        p->p.path("/auth/**").and()
+                        .uri("lb://TEAM3-AUTH"))
+                .route("team3-coupon",
+                        p -> p.path("/api/coupons/**").and()
+                                .uri("lb://TEAM3-COUPON"))
+                .route("team3-user",
+                        p -> p.path("/api/users/**").and()
+                                .uri("lb://TEAM3-USER"))
+                .build();
 
-
-//                .route("coupon",
-//                p->p.path("/coupons/**")
-//                        .uri("lb://COUPON"))
-//                .route("shop-service",
-//                        p->p.path("/shop-service/**")
-//                                .uri("lb://SHOP-SERVICE"))
     }
 }
