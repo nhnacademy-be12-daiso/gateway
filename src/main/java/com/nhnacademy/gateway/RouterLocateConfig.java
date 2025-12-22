fad5fb3b5aac8bd91d23f5c52e390fa6f2748c5c
한인혁 (InHyeok Han) <siezedayy@gmail.com>
2025-12-22 14:48:08 +0900

    feat: Update routing configuration to remove redundant payment path

--- src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
+++ src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
@@ -136,7 +136,7 @@ public class RouterLocateConfig {
                 // [Public] 장바구니/주문/결제
                 .route("team3-order-payment-public",
                         p -> p.path("/api/guest/**", "/api/carts/**",
-                                        "/api/orders/**", "/api/payments/**", "/payments/**")
+                                        "/api/orders/**", "/api/payments/**")
                                 .filters(f -> f
                                         .filter(authorizationHeaderFilter.apply(
                                                 new AuthorizationHeaderFilter.Config(ROLE_USER)))
