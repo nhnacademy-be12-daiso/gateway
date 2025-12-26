169213e1b97d44404c1a6b3d9863968e65d049e9
hetgwi01 <hetgwi03@gmail.com>
2025-12-26 10:42:09 +0900

    like 루트 복구

--- src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
+++ src/main/java/com/nhnacademy/gateway/RouterLocateConfig.java
@@ -141,7 +141,7 @@ public class RouterLocateConfig {
                 // [Public] 도서
                 .route("team3-booksearch",
                         p -> p.path("/api/books/**", "/api/reviews/**", "/api/likes/**",
-                                        "/api/v2/books/**")
+                                        "/api/v2/books/**", "/api/v2/likes/**")
                                 .filters(f -> f.requestRateLimiter(c -> c
                                         .setRateLimiter(searchRateLimiter())
                                         .setKeyResolver(userKeyResolver)))
